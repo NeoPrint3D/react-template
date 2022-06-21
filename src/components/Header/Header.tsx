66f8ae39e2e4a6726ba7c2e0d09bf1c9a8adf5ff
NeoPrint3D <drew@ronsman.com>
2022-06-21 15:30:58 -0500

    small changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,20 +9,20 @@ export default function Header() {
         <header className={`flex h-20  w-full items-center ${router.pathname === "/" ? "bg-transparent absolute" : " bg-white/50 backdrop-blur-3xl  relative"}`}>
             <div className="grid grid-cols-2 w-full">
                 <Link href="/" >
-                    <h1 className="flex items-center ml-4 text-white text-3xl font-mono">
-                        Vite Template
-                    </h1>
+                    <button className="flex items-center ml-4 text-white text-3xl font-mono">
+                        Next Template
+                    </button>
                 </Link>
                 <div className="flex gap-5 justify-end items-center mr-4">
                     <Link href="/" >
-                        <h1 className="text-white text-xl font-mono">
+                        <button className="text-white text-xl font-mono">
                             Home
-                        </h1>
+                        </button>
                     </Link>
                     <Link href="/about">
-                        <h1 className="text-white text-xl font-mono">
+                        <button className="text-white text-xl font-mono">
                             About
-                        </h1>
+                        </button>
                     </Link>
                 </div>
             </div>
