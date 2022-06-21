bf5a4db117880b34dc7a3512dedc456f99162d5d
NeoPrint3D <drew@ronsman.com>
2022-06-21 15:24:08 -0500

    stiff

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 const withPlugins = require("next-compose-plugins");
 const withPWA = require("next-pwa");
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
 module.exports = withPlugins([
-  [
-    withPWA({
-      pwa: {
-        dest: "public",
-        disable: process.env.NODE_ENV === "development",
-      }
-    })
-  ]
+  [withPWA({
+    pwa: {
+      dest: "public",
+      disable: process.env.NODE_ENV === "development",
+    }
+  })]
 ], nextConfig);
 
