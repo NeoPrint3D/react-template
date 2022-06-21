bb3688c4da0473dabd9ad64c2301aa707f58b36b
NeoPrint3D <drew@ronsman.com>
2022-06-21 14:58:21 -0500

    code minaturization

--- scripts/compress.js
+++ scripts/compress.js
@@ -1,21 +1,18 @@
 const sharp = require('sharp');
 const fs = require('fs');
-const outFiles = fs.readdirSync('./out');
-const images = fs.readdirSync('./public/images');
-const pages = outFiles.filter(file => file.endsWith('.html'));
+
+const [images, pages] = [fs.readdirSync('./public/images'), fs.readdirSync('./out').filter(file => file.endsWith('.html'))]
+
 images.forEach(async (image) => {
     const cleanName = image.split('.')[0];
     const imageSizeInKb = fs.statSync(`./public/images/${image}`).size / 1024
     const quality = imageSizeInKb < 25 ? 1 : 70
     fs.unlinkSync(`./out/images/${image}`);
-    await sharp(`./public/images/${image}`).
-        webp({ quality }).
-        toFile(`./out/images/${cleanName}.webp`);
+    await sharp(`./public/images/${image}`).webp({ quality }).toFile(`./out/images/${cleanName}.webp`);
     const newFileSizeInKb = fs.statSync(`./out/images/${cleanName}.webp`).size / 1024
     console.log("\x1b[36m%s\x1b[0m", ` ${cleanName} ${imageSizeInKb}kb -> ${newFileSizeInKb}kb`);
     pages.forEach(page => {
-        const html = fs.readFileSync(`./out/${page}`, 'utf8');
-        const newHtml = html.replace(`/images/${image}`, `/images/${cleanName}.webp`);
+        const newHtml = fs.readFileSync(`./out/${page}`, 'utf8').replace(`/images/${image}`, `/images/${cleanName}.webp`)
         fs.writeFileSync(`./out/${page}`, newHtml);
     })
 })
